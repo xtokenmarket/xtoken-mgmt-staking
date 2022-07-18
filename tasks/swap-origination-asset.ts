bdefa9b1aeccd2aa3f30158f4c70ca2e2e269c49
conache <C.conache@gmail.com>
2022-07-18 15:23:22 +0300

    Naming fixes

--- tasks/swap-origination-asset.ts
+++ tasks/swap-origination-asset.ts
@@ -9,9 +9,9 @@ import { Artifact } from "hardhat/types";
 const REVENUE_CONTROLLER_PROXY_ADDRESS = "0x37310ee55D433E51530b3efE41990360D6dBCFC3";
 
 task(SWAP_ORIGINATION_ASSET, "Swap origination asset once claimed")
-  .addParam("asset", "Asset token address")
+  .addParam("token", "Asset token address")
   .addParam("amount", "Amount of asset token swapped to XTK and staked")
-  .setAction(async ({ asset: assetAddress, amount: assetAmount }, { ethers, artifacts }) => {
+  .setAction(async ({ token: tokenAddress, amount: assetAmount }, { ethers, artifacts }) => {
     const revenueController = <RevenueController>(
       await ethers.getContractAt("RevenueController", REVENUE_CONTROLLER_PROXY_ADDRESS)
     );
@@ -21,9 +21,9 @@ task(SWAP_ORIGINATION_ASSET, "Swap origination asset once claimed")
     const erc20Artifacts: Artifact = await artifacts.readArtifact("IERC20");
     const xtkAddress = await revenueController.xtk();
     const xtk = <IERC20>await ethers.getContractAt(erc20Artifacts.abi, xtkAddress);
-    const assetToken = <IERC20>await ethers.getContractAt(erc20Artifacts.abi, assetAddress);
+    const assetToken = <IERC20>await ethers.getContractAt(erc20Artifacts.abi, tokenAddress);
 
-    const apiUrl = `https://api.1inch.io/v4.0/1/swap?fromTokenAddress=${assetAddress}&toTokenAddress=${xtkAddress}&amount=${assetAmount}&fromAddress=${REVENUE_CONTROLLER_PROXY_ADDRESS}&slippage=1&disableEstimate=true`;
+    const apiUrl = `https://api.1inch.io/v4.0/1/swap?fromTokenAddress=${tokenAddress}&toTokenAddress=${xtkAddress}&amount=${assetAmount}&fromAddress=${REVENUE_CONTROLLER_PROXY_ADDRESS}&slippage=1&disableEstimate=true`;
     const {
       data: { tx },
     } = await axios.get(apiUrl);
@@ -31,7 +31,7 @@ task(SWAP_ORIGINATION_ASSET, "Swap origination asset once claimed")
     const beforeClaimXtk = await xtk.balanceOf(stakingModuleAddress);
     const beforeClaimAsset = await ethers.provider.getBalance(revenueController.address);
 
-    await revenueController.swapAssetOnceClaimed(originationAddress, assetAddress, tx.data, tx.value);
+    await revenueController.swapAssetOnceClaimed(originationAddress, tokenAddress, tx.data, tx.value);
 
     const afterClaimXtk = await xtk.balanceOf(stakingModuleAddress);
     const afterClaimAsset = await ethers.provider.getBalance(revenueController.address);
